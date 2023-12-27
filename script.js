e3b7aa715c6d6b0d5a5d6db299875657cab42fa7
Deep Sankar De <78144390+Deep-dsd@users.noreply.github.com>
2023-12-27 11:31:02 +0530

    comment update

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const progressBarEl = document.getElementById("progress");
 const currentTimeEl = document.getElementById("current-time");
 const durationEl = document.querySelector(".duration");
 
+//Array containing Music Data
 const musicList = [
   {
     coverImg: "assets/img/img-1.jpg",
@@ -39,12 +40,16 @@ const musicList = [
   },
 ];
 let isPlaying = false;
+
+//Playing the Song
 const playSong = () => {
   audioEl.play();
   playBtnEl.classList.replace("fa-play", "fa-pause");
   playBtnEl.setAttribute("title", "Pause");
   isPlaying = true;
 };
+
+//Pausing the song
 const pauseSong = () => {
   audioEl.pause();
   playBtnEl.classList.replace("fa-pause", "fa-play");
@@ -53,10 +58,13 @@ const pauseSong = () => {
   isPlaying = false;
 };
 
+// Play and Pause the song
+
 playBtnEl.addEventListener("click", () => {
   !isPlaying ? playSong() : pauseSong();
 });
 
+//Loding the song in HTML
 function loadSong(song) {
   const { coverImg, name, title, audio } = song;
   titleEl.textContent = title;
@@ -64,8 +72,11 @@ function loadSong(song) {
   imgEl.src = coverImg;
   audioEl.src = audio;
 }
+
+//This Variable will keep the record of what song is loaded
 let songIndex = 0;
 
+//Going back to the Previous Song
 const prevSong = () => {
   if (songIndex === 0) {
     songIndex = musicList.length - 1;
@@ -75,6 +86,8 @@ const prevSong = () => {
   loadSong(musicList[songIndex]);
   playSong();
 };
+
+//Going to the Next song
 const nextSong = () => {
   if (songIndex === musicList.length - 1) {
     songIndex = 0;
@@ -87,12 +100,16 @@ const nextSong = () => {
 
 loadSong(musicList[songIndex]);
 
+//Updating the Progress bar dynamically, based on song duration, current song time
 const updateProgressBar = (e) => {
   if (isPlaying) {
+
+    //Changing the Progress bar width
     const { duration, currentTime } = e.target;
     const progressPercent = (currentTime / duration) * 100;
     progressBarEl.style.width = `${progressPercent}%`;
 
+    //Fetching the song length dynamically
     const durationMinutes = Math.floor(duration / 60);
     let durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
@@ -100,10 +117,13 @@ const updateProgressBar = (e) => {
     }
     const totalDuration = `${durationMinutes}:${durationSeconds}`;
 
+    //timeupdate event calls itself in 4 seconds interval. 
+    // So, sometimes if you click on the next song or prev song, It may show NaN.
+    //Because the data is not loaded yet, to overcome that we are using this condition.
     if (durationSeconds) {
-      durationEl.textContent = totalDuration;
+      durationEl.textContent = totalDuration; 
     }
-
+    //Fetching the long complition time dnamically
     const currentMinutes = Math.floor(currentTime / 60);
     let currentseconds = Math.floor(currentTime % 60);
     if (currentseconds < 10) {
@@ -114,18 +134,20 @@ const updateProgressBar = (e) => {
   }
 };
 
+// Change the Song complition time clicking on the progress bar
 function setProgressBar(e) {
   const width = this.clientWidth;
   const clickX = e.offsetX;
-  console.log(width, clickX);
   const { duration } = audioEl;
   audioEl.currentTime = (clickX / width) * duration;
 }
 
+//Event Listeners
 prevBtnEl.addEventListener("click", prevSong);
 nextBtnEl.addEventListener("click", nextSong);
+
+//This event keep tracks of change in audio
 audioEl.addEventListener("timeupdate", updateProgressBar);
 
 audioEl.addEventListener("ended", nextSong);
-
 progressContainerEl.addEventListener("click", setProgressBar);
